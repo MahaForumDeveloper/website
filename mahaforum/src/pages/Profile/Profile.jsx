0e31ab43b2a3aa3f0385871dbf28dc94892455fe
MSMazaya <msulthanmazaya@gmail.com>
2021-05-01 21:07:34 +0700

    add change

--- mahaforum/src/pages/Profile/Profile.jsx
+++ mahaforum/src/pages/Profile/Profile.jsx
@@ -4,13 +4,14 @@ import ProfileTabs from "../../components/ProfilePageComponents/ProfileTabs"
 import Search from "../../components/shared/Search"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 import ReviewMenu from "../../components/ProfilePageComponents/RightMenu/ReviewMenu"
 import RightNav from "../../components/ProfilePageComponents/RightMenu/RightNav"
-
 const Profile = () => {
     return (
         <Row>
             <Col>
+            {/*  */}
             </Col>
             <Col xs={7}>
                 <Search/>
@@ -20,15 +21,9 @@ const Profile = () => {
                 </div>
             </Col >
             <Col xs={3}>
-                
-            </Col>
-<<<<<<< HEAD
-=======
-            <Col xs={3}>
                 <RightNav />
                 <ReviewMenu />
             </Col>
->>>>>>> a7a0a5a06149610f1cfc1cad7418fcbe80d46bf7
         </Row>
     )
 }
